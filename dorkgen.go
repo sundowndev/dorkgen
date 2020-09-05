d7931bfbf28cb4944eaea3bff184d2c5e76af023
sundowndev <raphael@crvx.fr>
2020-09-05 17:20:56 +0200

    refactor: file structure and sub packages
    see #9

--- dorkgen.go
+++ dorkgen.go
@@ -1,15 +1,7 @@
 package dorkgen
 
-// EngineFactory is the main interface for
-// search engine implementations.
-type EngineFactory struct {
-	tags []string
-}
+import "github.com/sundowndev/dorkgen/googlesearch"
 
-func (e *EngineFactory) concat(tag string, value string, quotes bool) string {
-	if quotes {
-		return tag + "\"" + value + "\""
-	}
-
-	return tag + value
+func NewGoogleSearch() *googlesearch.GoogleSearch {
+	return googlesearch.New()
 }
