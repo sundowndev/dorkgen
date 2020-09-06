56b5a932e360e294e443c2bfc071fb38d6e88ccd
sundowndev <raphael@crvx.fr>
2020-09-06 17:39:04 +0200

    chore: add missing godocs

--- dorkgen.go
+++ dorkgen.go
@@ -5,10 +5,12 @@ import (
 	"github.com/sundowndev/dorkgen/googlesearch"
 )
 
+// NewGoogleSearch returns a new instance of GoogleSearch
 func NewGoogleSearch() *googlesearch.GoogleSearch {
 	return googlesearch.New()
 }
 
+// NewDuckDuckGo returns a new instance of DuckDuckGo
 func NewDuckDuckGo() *duckduckgo.DuckDuckGo {
 	return duckduckgo.New()
 }
